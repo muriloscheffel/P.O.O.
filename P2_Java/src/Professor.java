d637fb6de312585619a2110295abda03250463c8
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-02-28 13:57:02 +0000

    classe professor

---
+++ P2_Java/src/Professor.java
@@ -0,0 +1,9 @@
+public class Professor {
+    private String nome;
+    private int anosExp;
+
+    public Professor(String nome, int anosExp){
+        this.nome = nome;
+        this.anosExp = anosExp;
+    } 
+}
