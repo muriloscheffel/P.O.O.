662a82ebc8879ab94a8fb449770f2adc78c7d8fc
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-04-08 14:14:29 -0300

    Delete P13_Banco directory

--- P13_Banco/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        ContaComum conta = new ContaComum(1, 1000.0);
-        ContaPoupanca contaPoupanca = new ContaPoupanca(2, 200.00);
-        ContaLimite contaLimite = new ContaLimite(3, 500.0, 300.0);
-    }
-}
